e4bc9e2a1c8d4b433312f0edbb7d0a5f0676d2ad
Siphamandla Mzobe <siphamandla.mzobe@kcpivot.co.za>
2023-07-27 19:28:33 +0200

    added functionality to move a processed file to the processed dir

--- api/app/main.py
+++ api/app/main.py
@@ -114,6 +114,8 @@ def service_report_by_id(*, session: Session = Depends(get_session), id: int) ->
 UPLOAD_DIR = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), "uploads")
 
+PROCESSED_DIR = os.path.join(os.path.dirname(
+    os.path.realpath(__file__)), "processed")
 
 @app.post("/api/upload/")
 async def upload_file(file: UploadFile = File(...)):
@@ -133,24 +135,33 @@ async def upload_file(file: UploadFile = File(...)):
 
 @app.post("/api/processuploads/")
 async def process_uploads(*, session: Session = Depends(get_session)):
-    file_path = os.path.relpath(UPLOAD_DIR)
-    uploaded_file_names = get_files_in_directory(file_path)
+    try:
+        os.makedirs(PROCESSED_DIR, exist_ok=True)
 
-    for file_name in uploaded_file_names:
-        path_with_file_name = os.path.join(UPLOAD_DIR, file_name)
-        df = pd.read_excel(path_with_file_name, sheet_name='Sheet1')
+        file_path = os.path.relpath(UPLOAD_DIR)
+        uploaded_file_names = get_files_in_directory(file_path)
 
-        for index, row in df.iterrows():
-            new_data = ServiceReport(
-                attendance=row['attendance'], first_timers=row['first_timers'],
-                souls_saved=row["souls_saved"], service_review=row["service_review"],
-                service_type_id=row["service_type_id"], created_on=row["created_on"], 
-                updated_on=row["updated_on"], service_date=row["service_date"],)
+        for file_name in uploaded_file_names:
+            path_with_file_name = os.path.join(UPLOAD_DIR, file_name)
+            df = pd.read_excel(path_with_file_name, sheet_name='Sheet1')
 
-            session.add(new_data)
+            for index, row in df.iterrows():
+                new_data = ServiceReport(
+                    attendance=row['attendance'], first_timers=row['first_timers'],
+                    souls_saved=row["souls_saved"], service_review=row["service_review"],
+                    service_type_id=row["service_type_id"], created_on=row["created_on"], 
+                    updated_on=row["updated_on"], service_date=row["service_date"],)
 
-        session.commit()
-        session.close()
+                session.add(new_data)
+
+            session.commit()
+            session.close()
+
+            move_file_to_processed_dir(path_with_file_name)
+
+        return JSONResponse(content={"message": "Files processed successfully"})
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
 
 def get_files_in_directory(dir_path):
     res = []
@@ -162,6 +173,11 @@ def get_files_in_directory(dir_path):
             # add filename to list
             res.append(file_path)
     return res
+
+def move_file_to_processed_dir(file_path):
+    # Move the file from the source directory to the destination directory
+    processed_file = os.path.join(PROCESSED_DIR, os.path.basename(file_path))
+    os.rename(file_path, processed_file)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
