295498ca61e66c8596ed2122df16d8f5d31a192f
Siphamandla Mzobe <siphamandla.mzobe@kcpivot.co.za>
2023-08-26 22:59:38 +0200

    update the router tags

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -3,8 +3,8 @@ from fastapi import APIRouter
 from app.api.v1.endpoints import process_hop_report_uploads, service_report, service_report_type, uploads, hop_report
 
 api_router = APIRouter()
-api_router.include_router(service_report.router, prefix="/servicereport", tags=["ServiceReports"])
-api_router.include_router(service_report_type.router, prefix="/servicereporttypes", tags=["ServiceReportTypes"])
-api_router.include_router(uploads.router, prefix="/uploads", tags=["uploads"])
-api_router.include_router(hop_report.router, prefix="/hopreport", tags=["HopReports"])
-api_router.include_router(process_hop_report_uploads.router, prefix="/processhopreports", tags=["ProcessHopReports"])
+api_router.include_router(service_report.router, prefix="/servicereport", tags=["Service Reports"])
+api_router.include_router(service_report_type.router, prefix="/servicereporttypes", tags=["Service Report Types"])
+api_router.include_router(uploads.router, prefix="/uploads", tags=["Upload Files"])
+api_router.include_router(hop_report.router, prefix="/hopreport", tags=["Hop Reports"])
+api_router.include_router(process_hop_report_uploads.router, prefix="/processhopreports", tags=["Process Hop Reports File Uploads"])
